a2b125f67b81d9d8dfc6169da4c32c6437f70ebd
Matthew Auciello <matt.auciello1999@gmail.com>
2024-05-24 11:40:01 +1000

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Matthew Auciello on 24/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
